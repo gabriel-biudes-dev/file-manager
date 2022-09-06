9caa181f1d918804e2b5ee755c422a52350adfc5
Gabriel José Biudes <35238254+gabriel-biudes-dev@users.noreply.github.com>
2022-09-06 18:32:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, hashlib
 from pathlib import Path
 path_atual = Path()
 
@@ -21,23 +21,44 @@ def fileExists(f, p):
 def getText(f, p):
     if fileExists(f, p): return (p / f).read_text()
 
+def md5(filename):
+    md5_hash = hashlib.md5()
+    with open(filename,"rb") as f:
+        for byte_block in iter(lambda: f.read(4096),b""): md5_hash.update(byte_block)
+    return md5_hash.hexdigest()
+
 #Gets all files with certain extensions inside the directory if option = 1
 #Gets all files from the subdirectories too if option != 1
 #Returns just file name if opt2 == 1, returns absolute path for opt2 != 1
 def getFiles(p, ext, opt, opt2):
     files = []
     if opt == 1: flist = list(p.glob('*.' + ext))
-    else: flist = list(p.glob('**/*.' + ext))
+    else:
+        if len(ext) > 0: flist = list(p.glob('**/*.' + ext))
+        else: flist = list(p.glob('**/*'))
     for x in flist:
-        x = str(x)
-        if(opt2 == 1):
-            x = x.split('/')
-            files.append(x[len(x) - 1])
-        else: files.append(x)
+        if x.is_file():
+            x = str(x)
+            if(opt2 == 1):
+                x = x.split('/')
+                files.append(x[len(x) - 1])
+            else: files.append(x)
     return files
 
+def getBiggerStr(strlist):
+    higher = 0
+    for x in strlist:
+        if len(x) > higher: higher = len(x)
+    return higher
+
 def main():
-    p = getCurrentPath()
-    for x in getFiles(p, 'py', 2, 1):
-        print('FILE FOUND: ' + x)
+    higher = getBiggerStr(getFiles(getCurrentPath(), '', 2, 0))
+    f = open('data.txt', 'w')
+    f.close()
+    f = open('data.txt', 'a')
+    for x in getFiles(getCurrentPath(), '', 2, 0):
+        f.write(x)
+        for y in range(higher - len(x) + 1): f.write(' ')
+        f.write(md5(x) + '\n')
+    f.close()
 main()
